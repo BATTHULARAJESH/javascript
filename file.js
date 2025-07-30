104c61e82be09600ed6b62af1dfa4b7d53b00f85
BATTHULARAJESH <battlarajesh@gmail.com>
2025-07-30 11:58:55 +0530

    Add files via upload

---
+++ file.js
@@ -0,0 +1,7 @@
+alert("Using Javascript")
+document.writeln("Sample Page Admin")
+prompt("Enter into the Login Page!","Enter 1")
+let a=100;
+document.writeln(`age:${a}`)
+let sample_name="Rajesh"
+document.writeln(`Name:${sample_name}`)
